58b0447252370105aeee97f50e5ddc2da9a38cd6
tracer12 <vendetta12@naver.com>
2023-12-01 22:21:51 +0900

    Update calculatorDST3.h

--- calculatorDST3.h
+++ calculatorDST3.h
@@ -16,6 +16,9 @@ typedef struct queue3 {
 	int dist;
 }Queue3;
 
+extern Queue3 *q3;
+extern Queue3 *safety3;
+
 void bfs3(int x, int y);
 void dfs3(int x, int y, int npcX, int npcY);
 int ShortestDistance3(int npcX, int npcY);
